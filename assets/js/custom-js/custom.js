e0df2f52c4759c956702497f612ff1ef639780d2
Naresh Kumar <168510388+codebeans08@users.noreply.github.com>
2024-06-11 05:19:20 +0000

    Initial commit

---
+++ assets/js/custom-js/custom.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    // header toggle menu js
+    $(".mega-menu-area").hide();
+
+    $(".dropdown-toggle").click(function () {
+        $(".mega-menu-area").slideToggle("slow");
+    });
+
+    // Home Page Banner Slider
+    var swiper = new Swiper("#product_slider", {
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        spaceBetween: 16,
+        breakpoints: {
+            1024: {
+                slidesPerView: 5,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            0: {
+                slidesPerView: 2,
+            },
+        },
+    });
+
+    // offer_product_slider
+    var swiper = new Swiper("#offer_product_slider", {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        spaceBetween: -45,
+        slidesPerView: 2,
+    });
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+//scss compile
+
+node-sass -o ./assets/css/custom-css ./assets/scss/custom.scss
