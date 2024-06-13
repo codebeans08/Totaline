b05d20b960567aa9ee0945a5e50911275560f07e
Naresh Kumar <168510388+codebeans08@users.noreply.github.com>
2024-06-13 05:18:21 +0000

    Morning update 13-06-2024 10:48

--- assets/js/custom-js/custom.js
+++ assets/js/custom-js/custom.js
@@ -35,10 +35,77 @@ $(document).ready(function () {
             delay: 3000,
             disableOnInteraction: false,
         },
-        spaceBetween: -45,
-        slidesPerView: 2,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+            },
+            spaceBetween: -45,
+        },
     });
 });
+
+// faq page
+var scrollEventEnabled = true;
+
+$("#faq-menu a").on("click", function (event) {
+    event.preventDefault();
+
+    scrollEventEnabled = false;
+
+    $("#faq-menu a").removeClass("active");
+
+    $(this).addClass("active");
+
+    var targetId = $(this).attr("href").substring(1);
+    var targetElement = $("#" + targetId);
+    var offsetPosition = targetElement.offset().top - 20;
+
+    $("html, body").animate(
+        {
+            scrollTop: offsetPosition,
+        },
+        500,
+        function () {
+            scrollEventEnabled = true;
+        },
+    );
+});
+
+// Change active link on scroll
+$(window).on("scroll", function () {
+    if (!scrollEventEnabled) return;
+
+    var scrollPosition = $(window).scrollTop();
+    $("#faq-menu a").each(function () {
+        var targetId = $(this).attr("href").substring(1);
+        var targetElement = $("#" + targetId);
+        var targetOffsetTop = targetElement.offset().top - 50;
+
+        if (scrollPosition >= targetOffsetTop) {
+            $("#faq-menu a").removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+});
+
+
+
+
+var myMagicLine = new magicLine(document.querySelectorAll(".navbar-nav"), {
+    mode: "line",
+    animationCallback: function (el, params) {
+        anime({
+            targets: el,
+            left: params.left,
+            top: params.top,
+            width: params.width,
+            height: params.height,
+        });
+    },
+});
+myMagicLine.init();
 
 // artical details swiper
 new Swiper(".artical-details", {
